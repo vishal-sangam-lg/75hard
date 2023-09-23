90b2f7ad20b0206033403be8552dfc44a982139e
GD7777 <gaganofficials@gmail.com>
2023-09-23 13:19:28 +0530

    Header improvised

--- src/app/page.js
+++ src/app/page.js
@@ -3,17 +3,18 @@ import Description from "@/components/Body/Description";
 import Header from "@/components/Header";
 
 export default function Home() {
-	return (
-		<div className="h-screen gradient">
-			<div className="h-1/6">
-				<Header />
-			</div>
-			<div className="h-5/6">
-				<div className="min-h-full flex">
-					<Description />
-					<Checkboxes />
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="h-screen gradient">
+      <div className="h-1/6">
+        <Header />
+      </div>
+      <hr className="hr-1" />
+      <div className="h-5/6">
+        <div className="min-h-full flex">
+          <Description />
+          <Checkboxes />
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,65 @@
-import {Dela_Gothic_One } from "next/font/google";
+import { Dela_Gothic_One } from "next/font/google";
 import computer from "@/assets/computer.png";
 import Image from "next/image";
 import diet from "@/assets/diet.png";
 import dumbbell from "@/assets/dumbbell.png";
 import openbook from "@/assets/openbook.png";
-import waterbottle from "@/assets/waterbottle.png"
+import waterbottle from "@/assets/waterbottle.png";
 
 const DelaFont = Dela_Gothic_One({
-	  subsets : ['latin'],
-	  weight : '400',
-})
+  subsets: ["latin"],
+  weight: "400",
+});
 
 export default function Header() {
-	return <div className="min-h-full">
-	<div className="flex">
-	   <div className="leading-none px-8 pt-4 basis-7/12"   style={{fontFamily : 'Dela Gothic One', fontSize : "10vh"  }} > 75 HARD</div> 
-	   <div className =" px-4 pt-4 basis-1/12">  <Image src={computer} />  </div>
-	   <div className =" px-4 pt-4 basis-1/12">  <Image src={diet} />  </div>
-	   <div className =" px-4 pt-4 basis-1/12">  <Image src={dumbbell} />  </div>
-	   <div className =" px-4 pt-4 basis-1/12">  <Image src={openbook} />  </div>
-	   <div className =" px-4 pt-4 basis-1/12">  <Image src={waterbottle} />  </div>
-	</div>
-	</div>;
+  return (
+    <div className="min-h-full">
+      <div className="flex items-center">
+        <div
+          className="leading-none px-8 pt-4 basis-7/12 text-7xl"
+          style={{
+            fontFamily: "Dela Gothic One",
+          }}
+        >
+          {" "}
+          75 HARD
+        </div>
+        <div className=" px-4 pt-4 basis-1/12">
+          {" "}
+          <Image
+            src={computer}
+            className="object-contain max-w-full max-h-full"
+          />{" "}
+        </div>
+        <div className=" px-4 pt-4 basis-1/12">
+          {" "}
+          <Image
+            src={diet}
+            className="object-contain max-w-full max-h-full"
+          />{" "}
+        </div>
+        <div className=" px-4 pt-4 basis-1/12">
+          {" "}
+          <Image
+            src={dumbbell}
+            className="object-contain max-w-full max-h-full"
+          />{" "}
+        </div>
+        <div className=" px-4 pt-4 basis-1/12">
+          {" "}
+          <Image
+            src={openbook}
+            className="object-contain max-w-full max-h-full"
+          />{" "}
+        </div>
+        <div className=" px-4 pt-4 basis-1/12">
+          {" "}
+          <Image
+            src={waterbottle}
+            className="object-contain max-w-full max-h-full"
+          />{" "}
+        </div>
+      </div>
+    </div>
+  );
 }
